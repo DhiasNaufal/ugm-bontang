72e97f87577dd43475570985fa1257d4f3586462
DhiasNaufal <dhias@bangunindo.com>
2025-03-07 13:35:04 +0700

    Survey Count on dashboard

--- components/Dashboard/Pj/ProgressCard/SurverPbb.vue
+++ components/Dashboard/Pj/ProgressCard/SurverPbb.vue
@@ -1,9 +1,18 @@
 <template>
   <AppCardProgressCircular
     title="Survey PBB"
-    :value="0"
+    :value="surveyedCount"
     done-color="primary"
     :target-value="13000"
   />
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const surveyStore = useSurveyStore();
+const surveyedCount = ref(0);
+if (surveyStore.bidang_bontang_baru.length === 0) {
+  await surveyStore.getAllDoneBidangTanah();
+}
+surveyedCount.value = surveyStore.bidang_bontang_baru.filter(
+  (item: any) => item.status === true
+).length;
+</script>
