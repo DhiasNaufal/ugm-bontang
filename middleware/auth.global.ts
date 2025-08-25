f11110681e6f746f9ddcbe9399388d1b30c66bcd
DhiasNaufal <dhias@bangunindo.com>
2025-08-25 13:59:13 +0700

    Halaman Thankyou

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,16 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const user = await getCurrentUser();
+
+  // ✅ bikin whitelist route/layout yang public
+  const publicPages = ["thankyou"]; // nama route page
+  const publicLayouts = ["customPublic"]; // nama layout
+
+  // kalau masuk whitelist → skip auth
+  if (publicPages.includes(to.name as string)) return;
+  if (to.meta.layout && publicLayouts.includes(to.meta.layout as string))
+    return;
+
+  // rules auth biasa
   if (user && to.name === "auth-login") {
     return navigateTo("/");
   }
---
+++ pages/thankyou.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-white">
+    <p class="text-xl md:text-3xl font-semibold text-gray-800">
+      trims ya ges 😉
+    </p>
+    <span class="mt-2 text-sm md:text-base text-gray-400">from: dias</span>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  layout: "custom",
+  middleware: [],
+});
+</script>
